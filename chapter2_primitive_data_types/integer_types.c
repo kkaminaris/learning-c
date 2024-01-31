9ee87a238020c0303de180e0e1df0c6b85065270
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2024-01-31 21:02:03 +0200

    restructure and new: chapter2

---
+++ chapter2_primitive_data_types/integer_types.c
@@ -0,0 +1,15 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Size of signed char: %lu bytes\n", sizeof(signed char));
+    printf("Size of unsigned char: %lu bytes\n", sizeof(unsigned char));
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of short: %lu bytes\n", sizeof(short));
+    printf("Size of unsigned short: %lu bytes\n", sizeof(unsigned short));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of long long: %lu bytes\n", sizeof(long long));
+    printf("Max unsigned long long: %llu\n", ULLONG_MAX);
+    return 0;
+}
