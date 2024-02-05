2f3d791729b465350d878e90c553e68e2fc1f992
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2024-02-05 03:51:01 +0200

    structs lesson

--- chapter2_primitive_data_types/structs.c
+++ chapter2_primitive_data_types/structs.c
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
     struct pos point1 = { .x = 5, .y = 10 };
 
     printf("point1 x:%d\npoint1 y:%d\n", point1.x, point1.y);
+    printf("Size of position struct: %lu\n", sizeof(struct pos));
 
     // this struct is using bit fields, integer members of nonstandard sizes,
     // you cannot aquire their adress with &
@@ -22,6 +23,7 @@ int main(int argc, char **argv) {
     //printf("cannot access adress of card's suit: %d\n", &pick_a_card.suit);
 
     printf("King of Diamonds: %d %d\n", pick_a_card.suit, pick_a_card.face_value);
+    printf("Size of card struct: %lu\n", sizeof(struct card));
 
     return 0;
 
